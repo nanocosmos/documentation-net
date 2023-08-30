5629c36bd77488840365c3aeb5046a096357076d
Anna Matic <amatic@nanocosmos.de>
2023-08-30 07:40:53 +0000

    Bringing back GA

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,10 +47,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-        // gtag: {
-        //   trackingID: 'G-D55K07W445',
-        //   anonymizeIP: true,
-        // },
+        gtag: {
+          trackingID: 'G-D55K07W445',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
