922dd02f178d3a3c39509b833c174d732d9dff2b
Francisco oltra <foltra@gmail.com>
2023-09-06 15:16:42 -0600

    chore: documentation.nanocosmos.de -> docs.nanocosmos.de

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://documentation.nanocosmos.de',
+  url: 'https://docs.nanocosmos.de',
 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
