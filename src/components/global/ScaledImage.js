0907140d9a329ebbc799b2f022a186bd6055ac03
Maximilian Gertz <mgertz@nanocosmos.de>
2023-09-22 19:46:53 +0200

    added dynamic scaling edited headings

--- src/components/global/ScaledImage.js
+++ src/components/global/ScaledImage.js
@@ -1,51 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles.module.css'
 
 // IMPORTANT: image must be served from static source, therefore create a public subdirectory, like: assets/<used_dir>/public , place the needed asset there and be sure to sync with the staticDirectories property in docusaurus.config.js
-// scaling is available between 30-80% in 10% steps
+// scaling is available between 10-100% in 10% steps
 export const ScaledImage = ({image, alt, widthScaling, title, description}) => {
-    if (alt === undefined) 
-        alt = 'example'; 
+    useEffect(() => {
+        const handleResize = () => {
+            
+        };
+        window.addEventListener('resize', handleResize);
+
+        // Initialize window size
+        handleResize();
     
-    if (widthScaling === undefined) {
+        // Clean up event listener on component unmount
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    if (alt === undefined) 
+        alt = 'This is an example.'; 
+    
+    if (widthScaling === undefined)
+        widthScaling = 100;
+
+    if ( title === undefined ) {
         return (
-            <div className={styles['scaled-image-container']}> 
-                <img src={`/${image}`} alt={alt} />
-                <figcaption>{alt}</figcaption>
+            <div className={styles[`notext-container-centered`]}>
+                <img src={`/${image}`} className={styles[`image-${widthScaling}`]} alt={alt} />
+                <figcaption className={styles['scaled-image-caption']}>{alt}</figcaption>
             </div>
-        );
+            
+        )
     }
 
     return (
-        <div className={styles['scaled-image-container']}>
-            <div className={styles[`scaled-image-${widthScaling}`]}>
+        <div className={styles[`scaled-image-container-${widthScaling}`]}>
+            <div className={styles['scaled-image']}>
                 <img src={`/${image}`} alt={alt} />
-                <figcaption>{alt}</figcaption>
+                <figcaption className={styles['scaled-image-caption']}>{alt}</figcaption>
             </div>
-            { title && 
-                <div className={styles['scaled-image-textbox']}>
-                    <div>
-                        <ImageTitle title={title} />
-                        <ImageDescription description={description}/>
-                    </div>
+            <div className={styles['scaled-image-textbox']}>
+                <div className={styles["scaled-image-title"]}>
+                    {title}
                 </div>
-            }
+                <div className={styles['scaled-image-description']}>
+                    {description}
+                </div>
+            </div>
         </div>
     ) 
-}
-
-const ImageTitle = ({title}) => {
-    return (
-        <div className="badge">
-            <span>{title}</span>
-        </div>
-    ) 
-}
-
-const ImageDescription = ({description}) => {
-    return (
-        <div className={styles['scaled-image-description']}>
-            <span>{description}</span>
-        </div>
-    )   
 }
