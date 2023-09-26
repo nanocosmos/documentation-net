257d06e77d6ec323cae4f4377443c46a1dab24d2
Maximilian Gertz <mgertz@nanocosmos.de>
2023-09-26 11:21:38 +0200

    fixed responsiveness

--- src/components/global/ScaledImage.js
+++ src/components/global/ScaledImage.js
@@ -1,13 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import styles from './styles.module.css'
+import remark from 'remark';
+import remarkReact from 'remark-react';
+import remarkImages from 'remark-images';
+import styles from './styles.module.css';
+import ReactMarkdown from 'react-markdown';
 
 // IMPORTANT: image must be served from static source, therefore create a public subdirectory, like: assets/<used_dir>/public , place the needed asset there and be sure to sync with the staticDirectories property in docusaurus.config.js
 // scaling is available between 10-100% in 10% steps
 export const ScaledImage = ({image, alt, widthScaling, title, description}) => {
+    const [reArrange, setReArrange] = useState(false);
+
+    // check for undefined props 
+    if (alt === undefined) 
+        alt = 'This is an example.';
+    
+    if (widthScaling === undefined)
+        widthScaling = 100;
+
     useEffect(() => {
         const handleResize = () => {
-            
+            const parentContainers = document.querySelectorAll('div[class^="scaled-image-container"]');
+            console.log(parentContainers[0].offsetWidth, parentContainers[0].style.display);
+            if (parentContainers[0]?.offsetWidth < 800){
+                setReArrange(true);
+                parentContainers.forEach(parentContainer => {
+                    parentContainer.style.display = 'block';
+                })
+            } else {
+                setReArrange(false);
+                parentContainers.forEach(parentContainer => {
+                    parentContainer.style.display = 'grid';
+                })
+            }
         };
+
         window.addEventListener('resize', handleResize);
 
         // Initialize window size
@@ -15,15 +41,9 @@ export const ScaledImage = ({image, alt, widthScaling, title, description}) => {
     
         // Clean up event listener on component unmount
         return () => {
-          window.removeEventListener('resize', handleResize);
+            window.removeEventListener('resize', handleResize);
         };
     }, []);
-
-    if (alt === undefined) 
-        alt = 'This is an example.'; 
-    
-    if (widthScaling === undefined)
-        widthScaling = 100;
 
     if ( title === undefined ) {
         return (
@@ -31,24 +51,42 @@ export const ScaledImage = ({image, alt, widthScaling, title, description}) => {
                 <img src={`/${image}`} className={styles[`image-${widthScaling}`]} alt={alt} />
                 <figcaption className={styles['scaled-image-caption']}>{alt}</figcaption>
             </div>
-            
         )
     }
 
-    return (
-        <div className={styles[`scaled-image-container-${widthScaling}`]}>
-            <div className={styles['scaled-image']}>
-                <img src={`/${image}`} alt={alt} />
-                <figcaption className={styles['scaled-image-caption']}>{alt}</figcaption>
-            </div>
-            <div className={styles['scaled-image-textbox']}>
-                <div className={styles["scaled-image-title"]}>
-                    {title}
+    if (!reArrange){
+        return (
+            <div className={styles[`scaled-image-container-${widthScaling}`]}>
+                <div className={styles['scaled-image']}>
+                    <img src={`/${image}`} alt={alt} />
+                    <figcaption className={styles['scaled-image-caption']}>{alt}</figcaption>
                 </div>
-                <div className={styles['scaled-image-description']}>
-                    {description}
+                <div className={styles['scaled-image-textbox']}>
+                    <div className={styles["scaled-image-title"]}>
+                        {title}
+                    </div>
+                    <div className={styles['scaled-image-description']}>
+                        {description}
+                    </div>
                 </div>
             </div>
-        </div>
-    ) 
+        ) 
+    } else {
+        return (
+            <div className={styles[`scaled-image-container-${widthScaling}`]}>
+                <div className={styles['scaled-image-textbox']}>
+                    <div className={styles["scaled-image-title"]}>
+                        {title}
+                    </div>
+                    <div className={styles['scaled-image-description']}>
+                        {description}
+                    </div>
+                </div>
+                <div className={styles['scaled-image']}>
+                    <img src={`/${image}`} alt={alt} />
+                    <figcaption className={styles['scaled-image-caption']}>{alt}</figcaption>
+                </div>
+            </div>
+        )
+    }
 }
